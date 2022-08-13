c9416ecb407be76c50fff90789d4a51b7a175394
ElsieWakarima <96049425+ElsieWakarima@users.noreply.github.com>
2022-08-13 11:34:23 +0300

    Secret_message

---
+++ secret_message/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <form >
+    <h1>Secret Message</h1>
+    <input placeholder="Input Your Secret Message" class="message"><br>
+    <Button>Submit</Button>
+  </form>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+
+}
+input{
+  border-radius:100rem;
+  width:100%;
+  height:100%;
+}
+Button {
+  right:-100px;
+  top:20px;
+}
+
+
+/* h3 {
+  font-size: 1.2rem;
+} */
+
+/* .greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+} */
+</style>
